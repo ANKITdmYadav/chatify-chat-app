64b6100a731889c4119b2cf01dd51a62fc578e7f
Ankit yadav <ankityaduvansh1rao9818@gmail.com>
2026-08-09 22:51:24 +0530

    JWT cookie

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,7 +15,8 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    // secure: ENV.NODE_ENV === "development" ? false : true,
+    secure: false
   });
 
   return token;
